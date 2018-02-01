01cb2feaec1ddb4f9af93e34d5d6ad88782d1f0d
rafal-tarnow <rafal-tarnow@wp.pl>
2018-02-01 14:45:54 +0100

    dziala library_opengles_2 na androidzie

--- app/src/main/cpp/SystemAbstraction/system_abstraction.cpp
+++ app/src/main/cpp/SystemAbstraction/system_abstraction.cpp
@@ -96,10 +96,10 @@ void SystemAbstraction::onRenderFirstFrame()
 
 void SystemAbstraction::onRenderFrame()
 {
-   // mgr->DoFrame();
+    mgr->DoFrame();
 
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
     rectangle.model = glm::mat4(1);
     rectangle.projection = glm::mat4(1);
